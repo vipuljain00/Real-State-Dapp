258c7627a1ce8c194386087cb35a1bc521ad9870
vipuljain00 <arjavjain52@gmail.com>
2024-06-16 16:57:47 +0530

    building contract

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -6,12 +6,61 @@
 // global scope, and execute the script.
 const hre = require("hardhat");
 
-async function main() {
-
+const tokens = (n) => {
+  return ethers.utils.parseUnits(n.toString(), 'ether')
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
+async function main() {
+  //setup accounts
+  [buyer, seller, inspector, lender] = await ethers.getSigners()
+  //deploy real estate
+  const RealEstate = await ethers.getContractFactory("RealEstate")
+  const realEstate = await RealEstate.deploy()
+  await realEstate.deployed()
+  console.log(`Deployed Real State Contract at: ${realEstate.address}`)
+  console.log(`Minting 3 Properties...\n`)
+
+  for(let i = 0;i < 3;i++){
+    const transaction = await realEstate.connect(seller).mint(`https://ipfs.io/ipfs/QmXAGUpK9DngZMhuP7k6wSHmiNXJ8SjnGFDDcHLNShDwzq?filename=${i + 1}.json`)
+    await transaction.wait()  
+  }
+
+  const Escrow = await ethers.getContractFactory("Escrow")
+  escrow = await Escrow.deploy(
+      realEstate.address,
+      seller.address,
+      inspector.address,
+      lender.address
+  )
+  await escrow.deployed()
+  console.log(`Deployed Escrow Contract at: ${escrow.address}`)
+
+  for(let i = 0;i < 3;i++){
+    //approve properties...
+    let transaction = await realEstate.connect(seller).approve(escrow.address,i+1)
+    await transaction.wait()
+  }
+
+  //listing properties...
+  let transaction = await escrow.connect(seller).list(1, buyer.address, tokens(20), tokens(10))
+  await transaction.wait()
+
+  transaction = await escrow.connect(seller).list(2, buyer.address, tokens(15), tokens(5))
+  await transaction.wait()
+
+  transaction = await escrow.connect(seller).list(3, buyer.address, tokens(10), tokens(5))
+  await transaction.wait()
+
+  console.log("Finish")
+}
+
+
+
+
+
+
+
+
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
